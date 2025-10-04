41c6f52e087622e4f29645975a9ea5918ca690b3
Farhankerdiwangsa <jejakasurga@gmail.com>
2025-10-04 19:26:43 +0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
   const path = parsedUrl.pathname.replace(/^\/api/, "");
 
   // Gabung dengan alamat VPS
-  const backendUrl = "http://yoztampan.xintzy.me:2669" + path;
+  const backendUrl = "http://danzx-hosting.storedigital.web.id:3756" + path;
   console.log("Forwarding to:", backendUrl);
 
   try {
