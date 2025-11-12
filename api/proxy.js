d0327d234ea71fefade1889eed266fead151adc6
Farhankerdiwangsa <jejakasurga@gmail.com>
2025-11-12 13:56:26 +0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
   const path = parsedUrl.pathname.replace(/^\/api/, "");
 
   // Gabung dengan alamat VPS
-  const backendUrl = "http://s2-connect.pterokudesu.web.id:19881" + path;
+  const backendUrl = "http://private-server.privatserver.my.id:2027" + path;
   console.log("Forwarding to:", backendUrl);
 
   try {
